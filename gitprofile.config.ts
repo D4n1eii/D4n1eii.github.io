f3aee8a6f265567876b2257fc723e8248082f8e1
D4n1eii <daniel.andrade.martinsdd123bse@gmail.com>
2026-08-06 19:08:24 +0100

    Adding credit to original template provider

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -113,11 +113,19 @@ const CONFIG = {
 
   // Optional Footer. Supports plain text or HTML.
   footer: `Made with <a 
-      class="text-primary" href="https://github.com/arifszn/gitprofile"
-      target="_blank"
-      rel="noreferrer"
-    >GitProfile</a> and ❤️`,
-
+    class="text-primary" 
+    href="https://github.com/arifszn/gitprofile"
+    target="_blank"
+    rel="noreferrer"
+  >GitProfile</a> and ❤️
+  <br />
+  Template created by 
+  <a 
+    class="text-primary" 
+    href="https://github.com/arifszn"
+    target="_blank"
+    rel="noreferrer"
+  >arifszn</a>`,
   enablePWA: false,
 };
 
